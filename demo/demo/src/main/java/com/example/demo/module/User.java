17cc2f722d442225aa2d8d554d57dbff0c63433f
sandipGit51 <sandip1java@gmail.com>
2026-07-26 21:56:57 +0530

    my udpate on securoty

---
+++ demo/demo/src/main/java/com/example/demo/module/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String name;
+    private String email;
+}
